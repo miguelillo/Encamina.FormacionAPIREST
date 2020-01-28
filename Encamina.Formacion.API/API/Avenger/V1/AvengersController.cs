ec85c1beb01563912cfdce973f179839053f9214
Miguel Ángel Navarro Vera <manavarro@encamina.com>
2020-01-28 22:40:01 +0100

    Add project files.

---
+++ Encamina.Formacion.API/API/Avenger/V1/AvengersController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Encamina.Formacion.API.API.Avenger.V1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AvengersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Avengers()
+        {
+            return Ok("Hulk");
+        }
+    }
+}
